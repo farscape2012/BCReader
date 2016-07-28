875f1d19f8c67cc62a94d85e7fb8785fd40a8cf0
farscape2012 <farscape2012@users.noreply.github.com>
2016-07-28 21:11:17 +0300

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -5,7 +5,7 @@
 sudo apt-get install autoconf automake libtool
 sudo apt-get install libpng12-dev
 sudo apt-get install libjpeg62-dev
-sudo apt-get install libtiff4-dev
+sudo apt-get install libtiff4
 sudo apt-get install zlib1g-dev
 sudo apt-get install libicu-dev      # (if you plan to make the training tools)
 sudo apt-get install libpango1.0-dev # (if you plan to make the training tools)
