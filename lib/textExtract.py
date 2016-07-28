e58b2d0563f12de4e477757977a172e3fa8203d7
farscape2012 <farscape2012@users.noreply.github.com>
2016-07-28 19:36:42 +0300

    Create textExtract.py

---
+++ lib/textExtract.py
@@ -0,0 +1,10 @@
+import Image
+import pytesseract
+
+try:
+    import Image
+ except ImportError:
+    from PIL import Image
+    import pytesseract
+print(pytesseract.image_to_string(Image.open('test.png')))
+print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
