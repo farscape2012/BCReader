f66d469916a5dde387eb9c78087d2e4df78aee62
farscape2012 <farscape2012@users.noreply.github.com>
2016-07-28 21:09:36 +0300

    Create env-setup.sh

---
+++ env-setup.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env bash
+# author : Chengyu Liu
+
+sudo apt-get install autoconf automake libtool
+sudo apt-get install libpng12-dev
+sudo apt-get install libjpeg62-dev
+sudo apt-get install libtiff4-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libicu-dev      # (if you plan to make the training tools)
+sudo apt-get install libpango1.0-dev # (if you plan to make the training tools)
+sudo apt-get install libcairo2-dev   # (if you plan to make the training tools)
+sudo apt-get install tesseract-ocr
